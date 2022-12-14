a8cdf375b7d35a86e24c60568ded82805a9fa814
Nadiv N <nadiv.n.ft21@mail.umy.ac.id>
2022-12-14 17:07:20 +0700

    create method remove

--- CircularQueues_CSharp/Program.cs
+++ CircularQueues_CSharp/Program.cs
@@ -28,7 +28,23 @@ namespace CircularQueues_CSharp
                 FRONT = 0;
                 REAR = 0;
             }
+            else
+            {
+                /*If REAR is not at the last position of the array, then the value of
+                 * REAR is set to 0 that corresponds to the first position of the array*/
+                if (REAR == max - 1)
+                    REAR = 0;
+                else
+                    /*if REAR is not at the last position, then its value is incremented by one. */
+
+                REAR = REAR + 1;
+            }
+            /*Once the position of REAR is determined, the element is added at
+             * its proper place.*/
+            queue_array[REAR] = element;
         }
+        public void remove()
+        { }
 
     }
 }
