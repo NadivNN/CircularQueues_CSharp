622c0a55b852327a2e1a18c3dc566d33574d12cd
Nadiv N <nadiv.n.ft21@mail.umy.ac.id>
2022-12-14 16:47:38 +0700

    create variable FRONT and REAR

--- CircularQueues_CSharp/Program.cs
+++ CircularQueues_CSharp/Program.cs
@@ -4,7 +4,15 @@ namespace CircularQueues_CSharp
 {
     class Queues
     {
-
+        int FRONT, REAR, max = 5;
+        int[] queue_array = new int[5];
+        public Queues()
+        {
+            /* initializing the values of the variables REAR and FRONT to -1
+             * to indicate that queue is initially empty.*/
+            FRONT = -1;
+            REAR = -1;
+        }
 
     }
 }
